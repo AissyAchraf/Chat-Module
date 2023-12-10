0354e1ff250aeaebbc4bbf99e53458ca091f4d7a
Achraf AISSY <105090484+AissyAchraf@users.noreply.github.com>
2023-12-10 10:13:51 +0100

    init project

---
+++ src/main/java/sa/randerbox/chatmodule/ChatmoduleApplication.java
@@ -0,0 +1,13 @@
+package sa.randerbox.chatmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatmoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatmoduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sa/randerbox/chatmodule/ChatmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package sa.randerbox.chatmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatmoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
